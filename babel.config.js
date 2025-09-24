8bb69af12cd0213742635c391997f1f259849866
Apple <APPLE@MacBook-Pro.local>
2025-09-24 16:40:26 +0500

    marker issue resolve

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
-    '@babel/plugin-proposal-optional-chaining', // Optional chaining (?.)
+    '@babel/plugin-proposal-optional-chaining', // Optional chaining
     [
       '@babel/plugin-proposal-optional-chaining-assign',
       {
-        version: '2023-07'  // Add this version option
+        version: '2023-07' // Optional chaining assignment plugin version
       }
     ],
-    'react-native-reanimated/plugin'
+    'react-native-reanimated/plugin' // MUST BE LAST
   ]
 };
